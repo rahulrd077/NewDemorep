8a63f9e86d39b6acb8e891d7ced624e0c9ba122d
Rahul Das <rahulrd077@gmail.com>
2019-01-03 02:41:29 +0530

    Hey it is an new class i added just now

---
+++ src/main/java/demo/RahulMyProject/NewClass.java
@@ -0,0 +1,12 @@
+package demo.RahulMyProject;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hey");
+		// TODO Auto-generated method stub
+
+	}
+
+}
